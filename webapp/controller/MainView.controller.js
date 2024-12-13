87a1a168891afd3ea45a53b427750f1063ce90b1
edwines7738 <edwines7738@gmail.com>
2024-12-13 17:46:16 +0000

    modified:   webapp/controller/MainView.controller.js

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,7 +1,16 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
-function (Controller) {
+/**
+ * 
+ * @param {typeof sap.ui.core.mvc.Controller} Controller 
+ * @param {typeof sap.ui.model.Filter} Filter
+ * @param {typeof sap.ui.model.FilterOperator} FilterOperator
+ * @returns 
+ */
+function (Controller, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("eaes.invoices.controller.MainView", {
@@ -12,12 +21,27 @@ function (Controller) {
             oView.setModel(oJSONModel, "selectionScreen");
         },
         onFilter: function (){
+            const oData = this.getView().getModel("selectionScreen").getData();
+            let filters = [];
+            if(oData.ShipName !== ""){
+                filters.push(new Filter("ShipName", FilterOperator.Contains, oData.ShipName));
+            }
+            if(oData.CountryKey !== ""){
+                filters.push(new Filter("Country", FilterOperator.Contains, oData.CountryKey));
+            }
 
+            const oList = this.getView().byId("invoicesList");
+            const oBinding = oList.getBinding("items");
+            oBinding.filter(filters);
         },
         onClearFilter: function (){
             const oModelSelScreen = this.getView().getModel("selectionScreen");
             oModelSelScreen.setProperty("/ShipName", "");
             oModelSelScreen.setProperty("/CountryKey", "");
+
+            const oList = this.getView().byId("invoicesList");
+            const oBinding = oList.getBinding("items");
+            oBinding.filter([]);
         }
     });
 });
