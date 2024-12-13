ed3281213354810c3d3163f4175221c3e95177d2
edwines7738 <edwines7738@gmail.com>
2024-12-13 17:42:45 +0000

    modified:   ui5.yaml

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -6,7 +6,18 @@ function (Controller) {
 
     return Controller.extend("eaes.invoices.controller.MainView", {
         onInit: function () {
+            const oJSONModel = new sap.ui.model.json.JSONModel();
+            const oView = this.getView();
+            oJSONModel.loadData("./model/SelectionScreenMenu.json");
+            oView.setModel(oJSONModel, "selectionScreen");
+        },
+        onFilter: function (){
 
+        },
+        onClearFilter: function (){
+            const oModelSelScreen = this.getView().getModel("selectionScreen");
+            oModelSelScreen.setProperty("/ShipName", "");
+            oModelSelScreen.setProperty("/CountryKey", "");
         }
     });
 });
